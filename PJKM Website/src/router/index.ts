4917788c7ca996abe950d7bc6217af636a9ce869
SenpaiVR <SenpaiShichi@gmail.com>
2023-01-12 18:30:35 -0500

    vue3

---
+++ PJKM Website/src/router/index.ts
@@ -0,0 +1,14 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import release from '@/views/Release.vue'
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: release
+    }
+  ]
+})
+
+export default router
---
+++ PJKM Website/src/views/Release.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+	<v-img src="src/assets/Logos/PJKT-05(fade).png"/>
+</template>
+
+<style>
+
+</style>
